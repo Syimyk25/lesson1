c40167c0313bf96160498c7e8741538958fb23fe
Syimyk25 <csqvsr25@gmail.com>
2022-06-09 18:52:38 +0600

    new repo

---
+++ lesson3.py
@@ -0,0 +1,35 @@
+# GitHub - это онлайн сервис - который предоставляет 
+# услуги по хранению репозиториев и позволяет управлять ими
+
+# git - распределенная система контроля версий 
+# (позволяет записывать изменения, обновлять, получать и тд
+# или же вернуться к старым версиям проекта)
+
+"""
+git init - иницализация папки (все)
+git status   - статус
+
+git add <file>
+git add .  - следить за всем содержимым
+
+git rm --cached test.py  - перестать следить
+git commit -m "creted new repositiry"  - создает слепок, новые изменени ! в кавычках данные об изменениях
+git remote add origin https://github.com/Syimyk25/lesson.git
+git push origin master 
+
+# token!!! >>>   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
+
+git log - старые версии (коммитов)
+
+git remote add origin git@github.com:Syimyk25/lesson2.git - в эту ссылку добавь оригин
+git remote - посмотреть оригин
+git remote remove origin -  удалить
+
+git branch - ветка
+git chechout и название ветки- выйти с ветки
+"""
+
+
+# jfjnfnjfjnf
+# rrjrnfrnfrnfrjf
+# rfrurnfrnfirnf
